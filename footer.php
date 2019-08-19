e45164fccda21c3d5fe32fe5fff1017efe459f30
WolfwoodStryfe <54145463+WolfwoodStryfe@users.noreply.github.com>
2019-08-19 09:41:34 -0500

    otra prueba
    prueba de subida

---
+++ footer.php
@@ -0,0 +1,166 @@
+<!-- Footer -->
+<div class="w3l-footer">
+		<div class="container">
+         <div class="footer-info-agile">
+				<div class="col-md-2 footer-info-grid links">
+					<h4>Quick links</h4>
+					<ul>
+						       <li><a href="index.php">Home</a></li> 
+								<li><a href="about.php">About</a></li> 
+								<li><a href="codes.php">Services</a></li> 
+								<li><a href="gallery.php">Gallery</a></li> 
+								<li><a href="contact.php">Contact</a></li> 
+					</ul>
+				</div>
+				<div class="col-md-3 footer-info-grid address">
+					<h4>Address</h4>
+					<address>
+						<ul>
+							<li>Mexico United States 3000</li>
+							<li>40019 Honey Street</li>
+							<li>BO,Mexico</li>
+							<li>Telephone : +1 (734) 123-4567</li>
+							<li>Email : <a class="mail" href="mailto:mail@example.com">info(at)example.com</a></li>
+						</ul>
+					</address>
+				</div>
+				<div class="col-md-3 footer-grid">
+				   <h4>Instagram</h4>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="images/f1.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="images/f2.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+						<a href="#"><img src="images/f3.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="footer-grid-instagram">
+					<a href="#"><img src="images/f4.jpg" alt=" " class="img-responsive"></a>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="col-md-4 footer-info-grid">
+				<div class="connect-social">
+					<h4>Connect with us</h4>
+					<section class="social">
+                        <ul>
+							<li><a class="icon fb" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a class="icon tw" href="#"><i class="fa fa-twitter"></i></a></li>
+							
+							
+							<li><a class="icon pin" href="#"><i class="fa fa-pinterest"></i></a></li>
+							<li><a class="icon db" href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a class="icon gp" href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</section>
+
+				</div>
+					
+
+					
+				</div>
+				<div class="clearfix"></div>
+			</div>
+
+			<div class="connect-agileits newsletter">
+				<h4>Newsletter</h4>
+					<p>Subscribe to our newsletter and we will inform you about newest projects and promotions.
+					</p>
+					<form action="#" method="post" class="newsletter">
+						<input class="email" type="email" placeholder="Your email address..." required="">
+						<input type="submit" class="submit" value="Subscribe">
+					</form>
+			</div>
+	   </div>
+     </div>
+
+		
+			<div class="w3agile_footer_copy">
+				    <p>© 2017 Resort. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			</div>
+		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<!-- Dropdown-Menu-JavaScript -->
+			<script>
+				$(document).ready(function(){
+					$(".dropdown").hover(            
+						function() {
+							$('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+							$(this).toggleClass('open');        
+						},
+						function() {
+							$('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+							$(this).toggleClass('open');       
+						}
+					);
+				});
+			</script>
+		<!-- //Dropdown-Menu-JavaScript -->
+
+
+<script type="text/javascript" src="js/jquery.zoomslider.min.js"></script>
+		<!-- search-jQuery -->
+				<script src="js/main.js"></script>
+
+<!--/script-->
+	<script src="js/simplePlayer.js"></script>
+			<script>
+				$("document").ready(function() {
+					$("#video").simplePlayer();
+				});
+			</script>
+			<!-- flexSlider -->
+					<script defer src="js/jquery.flexslider.js"></script>
+					<script type="text/javascript">
+					$(window).load(function(){
+					  $('.flexslider').flexslider({
+						animation: "slide",
+						start: function(slider){
+						  $('body').removeClass('loading');
+						}
+					  });
+					});
+				  </script>
+<!--//script for portfolio-->
+				<!-- Calendar -->
+				<link rel="stylesheet" href="css/jquery-ui.css" />
+				<script src="js/jquery-ui.js"></script>
+				  <script>
+						  $(function() {
+							$( "#datepicker,#datepicker1,#datepicker2,#datepicker3" ).datepicker();
+						  });
+				  </script>
+			<!-- //Calendar -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+
+<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
+ <script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+<!--end-smooth-scrolling-->
+<!--js for bootstrap working-->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+</body>
+</html>
